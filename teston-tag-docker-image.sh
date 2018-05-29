6fdbaf4fb6991f52f555c8a264eac27edc585a7a
Rodrigo Martell <rodrigo.martell@gmail.com>
2018-05-29 16:18:00 +1000

    am

--- teston-tag-docker-image.sh
+++ teston-tag-docker-image.sh
@@ -7,7 +7,7 @@ TESTON_TAG_GIT_HASH="commit-${DRONE_COMMIT_SHA:0:8}"
 echo "${TESTON_TAG_GIT_HASH}"
 
 # Tag version only if in master and a version is set
-TESTON_COMMIT_TAG_VERSION=$$(git ls-remote -t origin | grep ${DRONE_COMMIT} | xargs | cut -d"/" -f3 | sed -e 's/\^{}//g')
+TESTON_COMMIT_TAG_VERSION=$(git ls-remote -t origin | grep ${DRONE_COMMIT} | xargs | cut -d"/" -f3 | sed -e 's/\^{}//g')
 if [[ "$DRONE_COMMIT_BRANCH" = "master" ]] && [[ "$DRONE_COMMIT_TAG" != "" ]]; then
     # Do the git magic here.
     TESTON_TAG_GIT_TAG="version-$DRONE_COMMIT_TAG"
