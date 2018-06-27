327afa9b3d712af41bb38918f9b416c5ea055ab0
Rodrigo Martell <rodrigo.martell@gmail.com>
2018-06-27 16:47:13 +1000

    test

--- make-release.js
+++ make-release.js
@@ -100,8 +100,7 @@ output zip file: ${config.outputZipFile}
         await execFile('git', ['commit', '-a', '-m', config.commitMessage]);
         await execFile('git', ['checkout', 'master']);
         await execFile('git', ['merge', '--no-ff', '-m', config.commitMessage, config.targetBranch]);
-        await execFile('git', ['tag', '-a', config.nextTagName]);
-        await execFile('git', ['push', config.nextTagName]);
+        await execFile('git', ['tag', '-a', '-m', config.nextTagName]);
         await execFile('git', ['push', '-v', 'origin', `refs/tags/${config.nextTagName}`]);
         await execFile('git', ['push', 'origin']);
 
