757e0c878c87adac9fff591b77cb23a3df6172b4
Rodrigo Martell <rodrigo.martell@gmail.com>
2018-09-26 12:29:29 +1000

    s

---
+++ index.js
@@ -0,0 +1,7 @@
+const gitflow = require('./lib/gitflow');
+const logger = require('./lib/logger');
+
+module.exports = {
+    gitflow,
+    logger,
+};
--- utils/build.js
+++
@@ -1,5 +0,0 @@
-const { exec } = require('shelljs');
-
-exec('rm -rf build');
-exec('mkdir -p build');
-exec('cp src/manifest.json build/manifest.json');
